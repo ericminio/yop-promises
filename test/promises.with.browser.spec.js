1283136b601b91455ad6d27822abfc31069228be
Eric Mignot <eric.mignot@gmail.com>
2017-03-31 09:31:34 -0400

    trying to get coverage of zombie tests

--- test/promises.with.browser.spec.js
+++ test/promises.with.browser.spec.js
@@ -1,11 +1,22 @@
 const Browser = require('zombie');
 var browser = new Browser();
+var fs = require('fs');
+var path = require('path');
+var istanbul = require('istanbul');
 var read = function(file) {
-    var path = require('path').join(__dirname, file);
-    var content = require('fs').readFileSync(path).toString();
+    var filePath = path.join(__dirname, file);
+    var content = fs.readFileSync(filePath).toString();
     return content;
 };
-
+var instrument = function(file) {
+    var global = (Function('return this;'))();
+    global['__coverage__'] = {};
+    var instrumenter = new istanbul.Instrumenter();
+    var code = read(file);
+    var instrumented = instrumenter.instrumentSync(code);
+    return instrumented;
+};
+var global = (Function('return this'))();
 describe('Promises in browser', function() {
 
     var app;
@@ -19,12 +30,22 @@ describe('Promises in browser', function() {
             }
             else {
                 response.setHeader('Content-Type', 'application/javascript');
-                response.write(read('../lib/promises.js'));
+                response.write(instrument('../lib/promises.js'));
             }
             response.end();
         }).listen(port, done);
     });
     afterEach(function() {
+        var code = "" +
+            "var global = (Function('return this;'))();" +
+            "var report = global['__coverage__'];" +
+            "report;";
+        var data = browser.evaluate(code);
+        var currentFile = __filename;
+        var dirname = currentFile.substring(0, currentFile.lastIndexOf(path.sep));
+        var filename = currentFile.substring(1+currentFile.lastIndexOf(path.sep));
+        var filePath = path.join(dirname, '../coverage', 'coverage.' + filename + '.json');
+        fs.writeFileSync(filePath, JSON.stringify(data));
         app.close();
     });
 
